6252e79f5405e7c3c6e84ee29d41031706e127b0
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-23 14:03:59 +0100

    first step in  DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, Sequence, String, DateTime
+from sqlalchemy import create_engine
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+
+
+Class DBStorage:
+    """ DBStorage class"""
+
+    __engine = None
+    __session = None
+   def __init__(self):
+        """ initialize of anew bd create the engine """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}
+                                    .format(getenv('HBNB_MYSQL_USER'), 
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')), 
+                                    pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+    
