0c9846932b13239c1426e79b973193aad6356f6f
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-23 17:55:28 +0100

    user

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__= 'amenities'
+    name = Column(String(1024), nullable=False)
