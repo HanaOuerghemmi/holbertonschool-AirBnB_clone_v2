c2125b20f481cef1d4de2fa878e4f6cd0dcc050f
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 13:01:26 +0100

    fix it

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship("City")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
