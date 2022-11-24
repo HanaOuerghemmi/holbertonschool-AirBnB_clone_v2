0b6b3b8fa6e915ee85527ab0d662778ba94b5456
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 16:59:19 +0100

    more trying to fix it

--- models/place.py
+++ models/place.py
@@ -19,7 +19,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-    reviews = relationship("Review", backref="place", cascade="delete")
+    """"reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
+                             viewonly=False)"""
     amenity_ids = []
