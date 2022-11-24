0655f98af6d3c8874d582fe089186106b88cfa19
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 15:17:57 +0100

    user

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'Review'
+    __tablename__ = 'review'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('places.id'), nullable=False)
