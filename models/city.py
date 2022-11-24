882828c5916aae67d5a24c5d27e7a984f8ee622e
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 11:50:01 +0100

    fix it

--- models/city.py
+++ models/city.py
@@ -3,10 +3,12 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage_t
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place",  backref="cities", cascade="delete")
+    if storage_t == 'db':
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place",  backref="cities", cascade="delete")
