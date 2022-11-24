51bdfc08844bb3b864217a6dad5b44ba66031588
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 17:29:07 +0100

    pep8

--- models/amenity.py
+++ models/amenity.py
@@ -4,8 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
 
+
 class Amenity(BaseModel, Base):
-    __tablename__= 'amenities'
+    __tablename__ = 'amenities'
     name = Column(String(1024), nullable=False)
     """place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)"""
--- models/state.py
+++ models/state.py
@@ -8,14 +8,15 @@ from models.base_model import Base
 from models.city import City
 import models
 
+
 class State(BaseModel, Base):
     """ State class """
-    
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") !="db":
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """return the list of city instance from file storage"""
