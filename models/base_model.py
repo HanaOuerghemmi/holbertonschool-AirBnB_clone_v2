216829efd7007c7b5f5a7b63d0f4a7151c8d5adf
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 10:47:11 +0100

    ini

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates  a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
-           for key, value in kwargs.items():
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
