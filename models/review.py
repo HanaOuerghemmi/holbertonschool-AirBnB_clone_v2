4ed562ba4e932621fe6302fbb2969ee7e953c71c
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 18:06:48 +0100

    pep8

--- models/review.py
+++ models/review.py
@@ -2,7 +2,9 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.place import Place
+from models.user import User
 
 
 class Review(BaseModel, Base):
@@ -10,4 +12,4 @@ class Review(BaseModel, Base):
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
