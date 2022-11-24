2b0c0fefc33efa3838aa1b6f4394dcbc399bf72c
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-24 11:08:35 +0100

    place

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,7 +10,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.state import User
+from models.user import User
 
 from sqlalchemy.orm import sessionmaker, scoped_session
 
