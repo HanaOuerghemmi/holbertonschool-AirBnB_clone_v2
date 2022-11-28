7a8769d383d1a7b72fd88800b8836d125f85e23b
HanaOuerghemmi <5004@holbertonschool.com>
2022-11-28 18:22:46 +0100

    hello flask

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,17 +32,23 @@ class DBStorage():
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
+
     def all(self, cls=None):
-        """"""
+        """this returns a dictionary of models"""
+        objects = dict()
+        all_classes = (User, State, City, Amenity, Place, Review)
         if cls is None:
-            objs = self.__session.query(
-                State, City, User, Review, Place, Amenity).all()
+            for class_type in all_classes:
+                query = self.__session.query(class_type)
+                for obj in query.all():
+                    obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                    objects[obj_key] = obj
         else:
-            objs = self.__session.query(cls.__class__.__name__)
-        o = {}
-        for obj in objs:
-            o[obj.__class__.__name__ + "." + obj.id] = obj
-        return o
+            query = self.__session.query(cls)
+            for obj in query.all():
+                obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                objects[obj_key] = obj
+        return objects
 
     def new(self, obj):
         """Add in database session."""
