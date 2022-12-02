45194abb946268ac3c08aabd8c2c66c573403e8a
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-02 10:20:56 +0100

    fix it

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -24,6 +24,7 @@ def cities_by_state():
     return render_template("8-cities_by_states.html", states=states,
                            cities=cities)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
